ebcc8ba2c0a1daf89e8e065946047944dd06acdf
EpicCactus94 <alimyust@gmail.com>
2023-12-18 09:52:43 -0500

    Gr12 FSE Dec 17 12 Home

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,8 +1,8 @@
 public class MainGame {
     private static final Dungeon dun = new Dungeon();
     private static final Player player = new Player(30,40);
-    protected static final int WID = 64*12;
-    protected static final int HGT= 64*12;
+    protected static final int WID = 1100;
+    protected static final int HGT= 700;
 
     public static void main(String[] args) {
         new Game3D(dun,player);
