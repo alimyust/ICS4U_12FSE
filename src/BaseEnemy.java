054c8e8cc0a539ebe66d89154f9832b60a303d84
EpicCactus94 <alimyust@gmail.com>
2024-01-10 00:42:28 -0500

    Gr12 FSE Jan 10 Home Got sprite rendering almost working. Just need to adjust the scale value and boundaries until it looks nice

--- src/BaseEnemy.java
+++ src/BaseEnemy.java
@@ -3,6 +3,8 @@ import java.awt.geom.AffineTransform;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 
+import static java.awt.Color.RED;
+import static java.awt.Color.red;
 import static java.lang.Math.PI;
 
 public class BaseEnemy extends ParentEntity {
@@ -11,84 +13,58 @@ public class BaseEnemy extends ParentEntity {
         int distFromPlayer = dist(x, player.x, y, player.y);
     }
 
+
+
+
     public void drawBaseEnemy(Graphics g, Player player, int HGT, int WID, RayCaster ray) {
         double angleRatio = -isPlayerLookingAt(player);
         int xPos = (int) (WID / 2 * angleRatio + WID / 2);
         if (xPos < 0 || xPos > WID) return;
 
         double eDist = Math.abs(dist(x, y, player.x, player.y));
-        int scale = 400 * 64;
+        double scale = Math.max(0.1, 1.0 / (eDist / 9) * 200); // Apply minimum scaling factor
+        int wid = 64;
+        int hgt = 128;
+        int sWid = (int) (wid * scale); // Scaled width
+        int sHgt = (int) (hgt * scale); // Scaled height
 
-        int wid;
-        int hgt;
-
-        // Set the original width and height
-        int originalWidth = 64;
-        int originalHeight = 128;
-
-        // Set the distance threshold for constant size
-        double constantSizeThreshold = 100.0;
-
-        if (eDist < constantSizeThreshold) {
-            // Keep the original size up to the threshold
-            wid = originalWidth;
-            hgt = originalHeight;
-        } else {
-            // Scale the size beyond the threshold
-            wid = (int) (1.0 / eDist * scale * 2);
-            hgt = (int) (1.0 / eDist * scale * 4);
-        }
-
-//        AffineTransform transform = new AffineTransform();
-//        transform.translate(xPos - wid / 2, HGT / 2);
-//        transform.scale((double) wid / originalWidth, (double) hgt / originalHeight);
-
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setStroke(new BasicStroke(ray.getDepth()));
-        for(int y=0; y< hgt; y++){
-            for(int x=0; x< 64/ray.getDepth(); x++){
-                int rDist = ray.getRayDist()[Math.min(xPos / ray.getDepth() + x*ray.getDepth(), ray.getRayDist().length - 1)];
-                if (eDist < rDist) {
-                    g.setColor(Color.red);
-                    System.out.println(x);
-                    g.drawLine(xPos +x * ray.getDepth(),HGT/2,
-                            xPos +x * ray.getDepth(),HGT/2 + 40);
-                }
+        // Adjust the vertical position based on the scaled height
+        int yPos = 300 + hgt - sHgt;
+        int yTopBound = (int) (200);  int yBotBound = (int) (HGT/2-hgt);
+        yPos = Math.max(yPos, yTopBound);
+        yPos = Math.min(yPos, yBotBound);
+        for (int x = 0; x < sWid; x++) {
+            double wDist = ray.getRayDist()[Math.min((xPos + x) / ray.getDepth(), ray.getRayDist().length - 1)];
+            if (wDist < eDist) continue; // draws a ray only when an enemy is closer than a wall
+            for (int y = 0; y < sHgt; y++) {
+                Color col = MainGame.enemyImgArr[0][(int) (y / scale)][(int) (x / scale)];
+                if (col.equals(Color.decode("#f8028a"))) continue;
+                g.setColor(col);
+                g.drawRect(xPos + x, yPos + y, 1, 1);
             }
         }
-//        for (int y = 0; y < originalHeight; y++) {
-//            for (int x = 0; x < originalWidth; x++) {
-//                int rDist = ray.getRayDist()[Math.min(xPos / ray.getDepth() + x / ray.getDepth(), ray.getRayDist().length - 1)];
-//                if (eDist < rDist) {
-//                    int imageX = (int) (x * wid/originalWidth );
-//                    int imageY = (int) (y * hgt/originalHeight);
-//                    g.setColor(MainGame.enemyImgArr[0][imageY][imageX]);
-//                    g2d.setTransform(transform);
-//                    g2d.drawLine(x, y, x, y);
-//                }
-//            }
-//        }
     }
 
 
-//        for (int y = 0; y < hgt ; y++) {
-//            for (int x = 0; x < wid; x++) {
-//                System.out.println(x + "," + y);
-//                g.setColor(MainGame.enemyImgArr[0][y][x]);
-//                g.fillRect(x, y, 1, 1);
-//            }
-//        }
-//        for(int i = 0; i < wid; i++) {
-//            int rDist = ray.getRayDist()[Math.min(xPos / ray.getDepth() + i, ray.getRayDist().length - 1)];
-//            if (eDist < rDist) {
-//                for(int j=0; j < 128; j++) {
-//                    g.setColor(MainGame.enemyImgArr[0][Math.min(i * 128/(wid)*64+j,8191)]);
-//                    g.drawLine(xPos -wid/2 + i , HGT/2 + j,
-//                            xPos -wid/2 + i , HGT/2 + j);
-//                }
-//            }
-//        }
-//    }
+/*        double eDist = Math.abs(dist(x, y, player.x, player.y));
+        double scale = 1.0/(eDist /2)* 800;
+        int wid = 64;
+        int hgt = 128;
+        int sWid = (int) (wid * scale); // Scaled width
+        int sHgt = (int) (hgt * scale); // Scaled height
+
+        for (int x = 0; x < wid; x++) {
+            double wDist = ray.getRayDist()[Math.min((xPos + x)/ray.getDepth(), ray.getRayDist().length - 1)];
+            if (wDist < eDist) continue; // draws a ray only when an enemy is closer than a wall
+            for (int y = 0; y <hgt ; y++) {
+                Color col = MainGame.enemyImgArr[0][y][x];
+                if (col.equals(Color.decode("#f8028a"))) continue;
+                g.setColor(col);
+                g.drawRect((int) (xPos +x * scale), (int) (200+y), 1, 1);
+            }
+        }*/
+
+
     public double isPlayerLookingAt(Player player) {
         double angle = Math.atan2(y - player.y, x - player.x); //slope between player and enemy
         double playerAngle = fixAng(player.getAngle());
